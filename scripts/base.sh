7f1fb63a91ae0239caaa283894c7f8bfae6b56a3
David Prandzioch <kontakt@davidprandzioch.de>
2016-04-10 09:02:32 +0200

    cleanup, no guest additions

--- scripts/base.sh
+++ scripts/base.sh
@@ -5,27 +5,80 @@ if [ ! -f /usr/local/sbin/pkg ]; then
     ASSUME_ALWAYS_YES=yes pkg bootstrap
 fi
 
-echo "virtualbox tools"
-pkg install -y virtualbox-ose-additions
-#echo 'ifconfig_em1="inet 10.6.66.42 netmask 255.255.255.0"' >> /etc/rc.conf
-sysrc vboxguest_enable=yes
-sysrc vboxservice_enable=yes
+# root ca
+pkg install -y ca_root_nss
 
-echo "sudo"
-pkg install -y sudo wget nano
+# vbox guest extensions
+#
+# nooope...
+#
+# https://www.freebsd.org/doc/handbook/virtualization-guest-virtualbox-guest-additions.html
+#
+# The VirtualBox™ guest additions provide support for:
+# * Clipboard sharing.
+# * Mouse pointer integration.
+# * Host time synchronization.
+# * Window scaling.
+# * Seamless mode.
+#
+# nothing of that is needed for vagrant and vboxsf doen't work anyway
+
+cat <<'EOF' >> /boot/loader.conf
+autoboot_delay="-1"
+EOF
+
+# sudo
+pkg install -y sudo nano
 echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /usr/local/etc/sudoers.d/vagrant
 
-echo "insecure ssh key"
+# vagrant ssh keys
 mkdir ~vagrant/.ssh
-chmod 700 ~vagrant/.ssh 
+chmod 700 ~vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O ~vagrant/.ssh/authorized_keys
-chown -R vagrant ~vagrant/.ssh
+chown -R vagrant:vagrant ~vagrant/.ssh
 chmod 600 ~vagrant/.ssh/authorized_keys
 
-echo "bash for root and vagrant"
+# root ssh keys
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /root/.ssh/authorized_keys
+chown -R root:root /root/.ssh
+chmod 600 /root/.ssh/authorized_keys
+
+# bash
 pkg install -y bash-static
 chsh -s /usr/local/bin/bash root
 chsh -s /usr/local/bin/bash vagrant
 
-echo "nfs client"
+# nfs client
 sysrc nfs_client_enable=yes
+sysrc mountd_flags="-r"
+sysrc rpcbind_enable=yes
+touch /etc/exports
+
+# cleanup packages
+pkg clean -ay
+
+# clean history
+cat /dev/null > /root/.history
+
+# disable root login again
+sed -i '' 's/PermitRootLogin yes/#PermitRootLogin no/g' /etc/ssh/sshd_config
+
+# clean up disk sapce
+rm -f /home/vagrant/*.iso
+rm -rf /tmp/*
+rm -rf /var/db/freebsd-update/files
+mkdir /var/db/freebsd-update/files
+rm -f /var/db/freebsd-update/*-rollback
+rm -rf /var/db/freebsd-update/install.*
+rm -rf /boot/kernel.old
+rm -rf /usr/src/*
+rm -f /*.core
+rm -f /var/db/dhclient.leases.*
+rm -f /var/db/pkg/repo-*.sqlite
+rm -f /etc/ssh/ssh_host_*
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -rf /EMPTY
+sync
